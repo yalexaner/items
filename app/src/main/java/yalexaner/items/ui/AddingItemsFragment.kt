d3e806e249f3dbf2c747f0bbb82457989cc38a96
yalexaner <mail.yalexaner@gmail.com>
2020-11-26 21:19:15 +0500

    creating a fragment for adding items amount

---
+++ app/src/main/java/yalexaner/items/ui/AddingItemsFragment.kt
@@ -0,0 +1,36 @@
+package yalexaner.items.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import yalexaner.items.R
+import yalexaner.items.data.ItemsViewModel
+import yalexaner.items.databinding.FragmentAddingItemsBinding
+
+class AddingItemsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentAddingItemsBinding.inflate(inflater, container, false)
+        val viewModel: ItemsViewModel by activityViewModels()
+
+        // enable button when there is a number
+        binding.itemsInput.doOnTextChanged { text, _, _, _ ->
+            binding.addItems.isEnabled = !text.isNullOrEmpty()
+        }
+
+        binding.addItems.setOnClickListener { view: View ->
+            viewModel.addItems(binding.itemsInput.text?.toString()?.toInt() ?: 0)
+            view.findNavController().navigate(R.id.action_addingItemsFragment_to_itemsFragment)
+        }
+
+        return binding.root
+    }
+}
--- app/src/main/java/yalexaner/items/ui/ItemsFragment.kt
+++ app/src/main/java/yalexaner/items/ui/ItemsFragment.kt
@@ -6,7 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import yalexaner.items.R
 import yalexaner.items.data.ItemsViewModel
 import yalexaner.items.databinding.FragmentItemsBinding
@@ -15,13 +16,16 @@ class ItemsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding: FragmentItemsBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_items, container, false)
-        val viewModel: ItemsViewModel by viewModels()
+        val viewModel: ItemsViewModel by activityViewModels()
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        binding.addNewOrder.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_itemsFragment_to_addingItemsFragment)
+        }
 
         return binding.root
     }
