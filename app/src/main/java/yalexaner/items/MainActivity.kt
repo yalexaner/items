748f772ffaba7d80c84f3a6bbe7eff3f0ccbdd7c
yalexaner <mail.yalexaner@gmail.com>
2020-12-02 20:20:48 +0500

    moving from fragments to activities

--- app/src/main/java/yalexaner/items/MainActivity.kt
+++ app/src/main/java/yalexaner/items/MainActivity.kt
@@ -1,12 +1,51 @@
 package yalexaner.items
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import yalexaner.items.data.OrderViewModel
+import yalexaner.items.data.OrderViewModelFactory
+import yalexaner.items.databinding.ActivityMainBinding
+import yalexaner.items.db.Order
+import java.time.OffsetDateTime
 
 class MainActivity : AppCompatActivity() {
 
+    private val newOrderActivityRequestCode = 1
+    private val orderViewModel: OrderViewModel by viewModels {
+        OrderViewModelFactory((application as OrdersApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.run {
+            viewModel = orderViewModel
+            lifecycleOwner = this@MainActivity
+        }
+
+        binding.addNewOrder.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewOrderActivity::class.java)
+            startActivityForResult(intent, newOrderActivityRequestCode)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newOrderActivityRequestCode) {
+            data?.getIntExtra(NewOrderActivity.EXTRA_REPLY, 0)?.let { items ->
+                val order = Order(0, OffsetDateTime.now(), items, 8)
+                orderViewModel.addItems(order)
+            }
+        }
     }
 }
---
+++ app/src/main/java/yalexaner/items/NewOrderActivity.kt
@@ -0,0 +1,40 @@
+package yalexaner.items
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.databinding.DataBindingUtil
+import yalexaner.items.databinding.ActivityNewOrderBinding
+
+class NewOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityNewOrderBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_new_order)
+        val itemsInput = binding.itemsInput
+        val addItems = binding.addItems
+
+        // enable button when there is a number
+        itemsInput.doOnTextChanged { text, _, _, _ ->
+            addItems.isEnabled = !text.isNullOrEmpty()
+        }
+
+        addItems.setOnClickListener {
+            val items = itemsInput.text.toString().toInt()
+            val replyIntent = Intent()
+
+            replyIntent.putExtra(EXTRA_REPLY, items)
+            setResult(Activity.RESULT_OK, replyIntent)
+
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "yalexaner.items.REPLY"
+    }
+}
