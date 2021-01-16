88cb111bc612db728cc03d49a1f98e5bedc5dcac
yalexaner <mail.yalexaner@gmail.com>
2021-01-16 14:55:45 +0500

    creating sqlite migration from 1 to 2

--- app/src/main/java/yalexaner/items/OrdersApplication.kt
+++ app/src/main/java/yalexaner/items/OrdersApplication.kt
@@ -1,15 +1,11 @@
 package yalexaner.items
 
 import android.app.Application
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import yalexaner.items.db.OrderRepository
 import yalexaner.items.db.OrderRoomDatabase
 
 class OrdersApplication : Application() {
 
-    private val applicationScope = CoroutineScope(SupervisorJob())
-    private val database by lazy { OrderRoomDatabase.getDatabase(this, applicationScope) }
-
+    private val database by lazy { OrderRoomDatabase.getDatabase(this) }
     val repository by lazy { OrderRepository(database.orderDao()) }
 }
--- app/src/main/java/yalexaner/items/db/OrderRoomDatabase.kt
+++ app/src/main/java/yalexaner/items/db/OrderRoomDatabase.kt
@@ -1,18 +1,14 @@
 package yalexaner.items.db
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import java.time.OffsetDateTime
 
-@Database(entities = [Order::class], version = 1, exportSchema = false)
+@Database(entities = [Order::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class OrderRoomDatabase : RoomDatabase() {
 
@@ -22,40 +18,33 @@ abstract class OrderRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: OrderRoomDatabase? = null
 
-        fun getDatabase(context: Context, scope: CoroutineScope): OrderRoomDatabase {
+        fun getDatabase(context: Context): OrderRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     OrderRoomDatabase::class.java,
                     "orders"
-                ).addCallback(OrderDatabaseCallback(scope)).build()
+                ).addMigrations(MIGRATION_1_2).build()
 
                 INSTANCE = instance
 
                 instance
             }
         }
-    }
 
-    private class OrderDatabaseCallback(private val scope: CoroutineScope) :
-
-        RoomDatabase.Callback() {
-
-        override fun onCreate(db: SupportSQLiteDatabase) {
-            super.onCreate(db)
-            INSTANCE?.let { database ->
-                scope.launch {
-                    populateDatabase(database.orderDao())
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("ALTER TABLE orders RENAME COLUMN items TO items_collected")
+//                database.execSQL("ALTER TABLE orders ADD COLUMN items_ordered INTEGER")
+                with(database) {
+                    execSQL("BEGIN TRANSACTION")
+                    execSQL("CREATE TABLE orders_new (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, items_collected INTEGER NOT NULL, items_ordered INTEGER NOT NULL, date TEXT NOT NULL, coast_per_item INTEGER NOT NULL)")
+                    execSQL("INSERT INTO orders_new SELECT id, items, items, date, cpi FROM orders")
+                    execSQL("DROP TABLE orders")
+                    execSQL("ALTER TABLE orders_new RENAME TO orders")
+                    execSQL("COMMIT")
                 }
             }
-        }
-
-        @RequiresApi(Build.VERSION_CODES.O)
-        private suspend fun populateDatabase(orderDao: OrderDao) {
-            orderDao.deleteAll()
-
-            orderDao.insert(Order(0, OffsetDateTime.now(), 16, 8))
-            orderDao.insert(Order(1, OffsetDateTime.now(), 32, 8))
         }
     }
 }
