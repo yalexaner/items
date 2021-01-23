f8d74c8cda5b9a0d1fbf9702b1b9fea97277d62e
yalexaner <mail.yalexaner@gmail.com>
2021-01-23 19:11:24 +0500

    hiding activities and fragments

--- app/src/main/java/yalexaner/items/ui/main/OrdersListFragment.kt
+++ app/src/main/java/yalexaner/items/ui/main/OrdersListFragment.kt
@@ -1,41 +1,41 @@
 package yalexaner.items.ui.main
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import yalexaner.items.OrdersApplication
-import yalexaner.items.R
-import yalexaner.items.data.OrderListAdapter
-import yalexaner.items.data.OrderViewModel
-import yalexaner.items.data.OrderViewModelFactory
-import yalexaner.items.databinding.FragmentOrdersListBinding
-
-class OrdersListFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        val binding: FragmentOrdersListBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_orders_list, container, false)
-        val orderViewModel: OrderViewModel by activityViewModels {
-            OrderViewModelFactory((activity?.application as OrdersApplication).repository)
-        }
-        val orderAdapter = OrderListAdapter(this.requireContext(), orderViewModel)
-
-        binding.ordersList.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = orderAdapter
-        }
-
-        orderViewModel.orders.observe(viewLifecycleOwner) { orders ->
-            orderAdapter.submitList(orders)
-        }
-
-        return binding.root
-    }
-}
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.databinding.DataBindingUtil
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.activityViewModels
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import yalexaner.items.OrdersApplication
+//import yalexaner.items.R
+//import yalexaner.items.data.OrderListAdapter
+//import yalexaner.items.data.OrderViewModel
+//import yalexaner.items.data.OrderViewModelFactory
+//import yalexaner.items.databinding.FragmentOrdersListBinding
+//
+//class OrdersListFragment : Fragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+//    ): View {
+//        val binding: FragmentOrdersListBinding =
+//            DataBindingUtil.inflate(inflater, R.layout.fragment_orders_list, container, false)
+//        val orderViewModel: OrderViewModel by activityViewModels {
+//            OrderViewModelFactory((activity?.application as OrdersApplication).repository)
+//        }
+//        val orderAdapter = OrderListAdapter(this.requireContext(), orderViewModel)
+//
+//        binding.ordersList.apply {
+//            layoutManager = LinearLayoutManager(context)
+//            adapter = orderAdapter
+//        }
+//
+//        orderViewModel.orders.observe(viewLifecycleOwner) { orders ->
+//            orderAdapter.submitList(orders)
+//        }
+//
+//        return binding.root
+//    }
+//}
